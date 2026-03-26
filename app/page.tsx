da735285905ee991be81a1ff5aa3f23178c90c19
kzhhyu <keziahlonoy@gmail.com>
2026-03-26 21:01:42 +0800

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from '@/components/Hero'
 import About from '@/components/About'
-import Work from '@/components/Work'
+import Projects from '@/components/Projects'
 import Contact from '@/components/Contact'
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     <>
       <Hero />
       <About />
-      <Work />
+      <Projects />
       <Contact />
     </>
   )
