678e57bdadbe5d9e6dc1431821d48dc9ebd6cab1
kzhhyu <keziahlonoy@gmail.com>
2026-03-26 21:00:11 +0800

    commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
     extend: {
